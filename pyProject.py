70e71cf4813e48c77a3edc86a9d70a06bbad6b8c
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-10-04 08:34:22 -1000

    python interacting with sql

--- pyProject.py
+++ pyProject.py
@@ -1,22 +1,26 @@
 import MySQLdb
+
 #-----MySQLdb SETUP
 ip = "192.168.0.131"
 user = "erizho21"
 passwd = "zhou"
 dtbase = "erizho21"
 db = MySQLdb.connect(ip, user, passwd, dtbase)
-cur = db.cursor(MySQLdb.cursors.Cursor)
+cur = db.cursor(MySQLdb.cursors.DictCursor)
 
 #-----SELECT BLOCK
 def selectData ():
+	#Asks for showing data in database
 	global choiceA
-	choiceA = input('Do you want to see the students list? [Y/N]')
+	choiceA = input('> Do you want to see the students list? [Y/N]')
 	if choiceA == 'Y':
 		sql = "SELECT name,age,gradeLevel FROM students"
 		cur.execute(sql)
 		rows = cur.fetchall()
+		print()
 		for row in rows:
-			print(row)
+			print("Name: " + row["name"] + " Age: " + str(row["age"]) + " Grade Level: " + str(row["gradeLevel"]))
+			print()
 		else:
 			print("-Search Done-")
 		askAdd()
@@ -25,13 +29,15 @@ def selectData ():
 	else:
 		print('Input is case sensitive')
 		selectData()
-
+		
+#-----ASK BLOCK
 def askAdd ():
+	#Asks for adding new data
 	global choiceB
 	if choiceA == 'Y':
-		choiceB = input('Do you want to add a student [Y/N]')
+		choiceB = input('> Do you want to add a student [Y/N]')
 	elif choiceA == 'N':
-		choiceB = input('Do you still want to add a student? [Y/N]')
+		choiceB = input('> Do you still want to add a student? [Y/N]')
 	if choiceB == 'Y':
 		insertData()
 	elif choiceB == 'N':
@@ -39,78 +45,79 @@ def askAdd ():
 	else:
 		print('Input is case sensitive')
 		askAdd()
-
+		
+#-----DATA BLOCK
 def insertData ():
+	#Asks for User info and prevents misinputs
 	def checker1():
-		global name
-		nameCheck = input('Name: ')
+		nameCheck = input('> Name: ')
 		if len(nameCheck) != 0:
-			name = nameCheck
+			return nameCheck
 		else:
 			print('Input can\'t be empty')
 			checker1()
 	def checker2():
-		global age
-		ageCheck = input('Age: ')
+		ageCheck = input('> Age: ')
 		if ageCheck.isnumeric():
-			age = int(ageCheck)
+			return int(ageCheck)
 		else:
 			print('Input is not a number')
 			checker2()
 	def checker3():
-		global gradeLevel
-		gradeLevelCheck = input('Grade Level: ')
+		gradeLevelCheck = input('> Grade Level: ')
 		if gradeLevelCheck.isnumeric():
-			gradeLevel = int(gradeLevelCheck)
+			return int(gradeLevelCheck)
 		else:
 			print('Input is not a number')
 			checker3()
-	checker1()
-	checker2()
-	checker3()
+	name = checker1()
+	age = checker2()
+	gradeLevel = checker3()
 	dataCheck(name,age,gradeLevel)
-
+	
+#-----CHECK BLOCK
 def dataCheck (p1,p2,p3):
+	#Checks user inputs in case of wrong datatype or over rows's data limits
 	validCheck = 1
 	print('Checking inputs...')
 	
 	print(type(p1))
-	if type(p1) == str and len(p1) < 22 and len(p1) != 0:
-		print('Input Valid')
+	if type(p1) == str and len(p1) <= 22 and len(p1) != 0:
+		print('-Input Valid-')
 	elif len(p1) > 22:
-		print('Input is too long')
+		print('-Input is too long-')
 		validCheck = 0
 	elif len(p1) == 0:
-		print('Input is empty')
+		print('-Input is empty-')
 		validCheck = 0
 	else:
-		print('Input Invalid')
+		print('-Input Invalid-')
 		validCheck = 0
 	
 	print(type(p2))
 	if type(p2) == int and p2 < 100 and p2 > 1:
-		print('Input Valid')
+		print('-Input Valid-')
 	elif p2 > 99:
-		print('Input is past max limit')
+		print('-Input is past max limit-')
 		validCheck = 0
 	elif p2 < 1:
-		print('Input is past min limit')
+		print('-Input is past min limit-')
 		validCheck = 0
 	else:
-		print('Input Invalid')
+		print('-Input Invalid-')
 		validCheck = 0
 	
 	print(type(p3))
 	if type(p3) == int and p3 <= 12 and p3 > 0:
-		print('Input Valid')
+		print('-Input Valid-')
 	elif p3 > 12:
-		print('Input is past max grade level')
+		print('-Input is past max grade level-')
 		validCheck = 0
 	elif p3 <= 0:
-		print('Input is not a grade level')
+		print('-Input is not a grade level-')
 		validCheck = 0
 	else:
-		print('Input Invalid')
+		print('-Input Invalid-')
 		validCheck = 0
 	
 	if validCheck == 0:
@@ -118,23 +125,25 @@ def dataCheck (p1,p2,p3):
 		askAdd()
 	elif validCheck == 1:
 		dataAdd(p1,p2,p3)
-	
+		
+#-----INSERT BLOCK
 def dataAdd (p1,p2,p3):
+	#Double check with user and inserts data
 	val = (p1,p2,p3)
-	print(val)
-	userCheck = input('Is this who you want to add [Y/N] ')
+	userCheck = input(f'> {val} Is this who you want to add [Y/N] ')
 	if userCheck == 'Y':
-		sql = "INSERT INTO students (name,age,gradeLevel) VALUES (%s, %s, %s)"
-		cur.execute(sql, val)
+		sql = f"INSERT INTO students (name,age,gradeLevel) VALUES ('{p1}',{p2},{p3})"
+		cur.execute(sql)
 		db.commit()
-		print('Information uploaded')
+		print('-Information uploaded-')
 	elif userCheck == 'N':
 		choiceA = 'N'
 		askAdd()
 	else:
 		print('Input is case sensitive')
 		dataAdd(p1,p2,p3)
-	
+
+#functions loaded, start first function
 selectData()
 
 #-----MySQLdb CLOSE
