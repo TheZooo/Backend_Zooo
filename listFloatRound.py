b8c7ceea59d2a527ad6620fd90f49bd9af98081e
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-11-18 08:24:31 -1000

    Update listFloatRound.py

--- listFloatRound.py
+++ listFloatRound.py
@@ -1,27 +1,36 @@
 myStringList = []
 myFloatTuple = (3.141592, 8.249561, 21.0219421)
-myListArray = [[1,5,8,3], [3,1,1,4,6], [9,3,3,2,8,5,4]]
+myItemTupleArray = (
+('Apple', 'Orange', 'Banana', 'Kiwi', 'Watermelon', 'Cantaloupe'), 
+('Durian', 'Starfruit', 'Lychee', 'Cherry')
+)
+myListArray = [
+[1,5,8,3], 
+[3,1,1,4,6], 
+[9,3,3,2,8,5,4]
+]
 
 def sortStrLen(lengths):
 	#Used as a key in sort to sort by string length
 	return len(lengths)
-
+	
 def rounder(*flo):
 	#Goes through inputted numbers and rounds to the hundrenths
 	for x in flo:
 		return round(x, 2)
 	
-def alphebatizer(*items):
-	#Goes through the inputted strings and appends to a list
-	for string in items:
-		myStringList.append(string)
-	#String List is sorted by alphabet and length, then printed out
-	myStringList.sort()
-	print(myStringList)
-	myStringList.sort(reverse=True, key=sortStrLen)
-	print(f'Longest Word: {myStringList[0]}')
-	print(f'Shortest Word: {myStringList[-1]} \n')
-	myStringList.clear()
+def alphebatizer(*tupArr):
+	#Goes through the inputted array
+	for subTup in tupArr:
+		#Through the Sub Tuples
+		for strings in subTup:
+			#Through the strings in the tuples
+			for item in strings:
+				myStringList.append(item)
+			#String List is sorted by alphabet, then length, and printed out
+			print(sorted(myStringList))
+			print(f'Longest Word: {sorted(myStringList, reverse=True, key=sortStrLen)[0]} \n Shortest Word: {sorted(myStringList, reverse=True, key=sortStrLen)[-1]} \n')
+			myStringList.clear()
 
 def arrInfo(*mainArr):
 	#Goes through the inputted array
@@ -29,17 +38,12 @@ def arrInfo(*mainArr):
 		print(f'Main Array: {subArr} \n')
 		#Goes through sub arrays to find the index, mean, max, and min
 		for value in subArr:
-			print(f'Sub-List {subArr.index(value) + 1}')
-			print(f'Sub-Length: {len(value)}')
-			print(f'Mean: {round(sum(value) / len(subArr), 2)}')
 			numList = value.copy()
 			numList.sort()
-			print(f'Max Value: {numList[-1]}')
-			print(f'Min Value: {numList[0]} \n')
+			print(f'Sub-List {subArr.index(value) + 1} \n Sub-Length: {len(value)} \n Mean: {round(sum(value) / len(subArr), 2)} \n Max Value: {numList[-1]} \n Min Value: {numList[0]} \n')
 
 #Triggering Functions
 for num in myFloatTuple:
 	print(f'{rounder(num)} Rounded \n')
-alphebatizer('Apple', 'Orange', 'Banana', 'Kiwi', 'Watermelon', 'Cantaloupe')
-alphebatizer('Durian', 'Starfruit', 'Lychee', 'Cherry')
+alphebatizer(myItemTupleArray)
 arrInfo(myListArray)
