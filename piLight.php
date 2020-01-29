c0059e6df9f3c9ec13686b8a8544776755fdd48b
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-01-29 08:15:49 -1000

    Update piLight.php

--- piLight.php
+++ piLight.php
@@ -1,5 +1,7 @@
 <?php
+//Recieve an XMLHTTP request from piWeb.php
 $catch = $_REQUEST["turnOnOff"];
+//Usual sql info
 $servername = "";
 $username = "piControl";
 $password = "f103";
@@ -9,14 +11,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+//Updates the info in a database to switch on or off the LED
 if ($catch == 0) {
     $sql = "UPDATE lightState SET switchLight='0'";
 } else if ($catch == 1) {
     $sql = "UPDATE lightState SET switchLight='1'";
 }
-
 $result = $conn->query($sql);
 
+//Close sql connection
 $conn->close();
 ?>
